037be2b1293df7e10ad3516125529cf75b3430b5
valparahiso <simovuillermoz@gmail.com>
2022-10-21 14:43:52 +0200

    Keystone version is working, starting a new branch to modify the SM code

--- source.sh
+++ source.sh
@@ -1,3 +1,5 @@
-export RISCV=/home/simo/Scrivania/Tesi/keystone_edit/riscv64
-export PATH=/home/simo/Scrivania/Tesi/keystone_edit/riscv64/bin:$PATH
-export KEYSTONE_SDK_DIR=/home/simo/Scrivania/Tesi/keystone_edit/sdk/build64
+export RISCV=/home/simo/keystone_edit/riscv64
+export PATH=/home/simo/keystone_edit/riscv64/bin:$PATH
+export KEYSTONE_SDK_DIR=/home/simo/keystone_edit/sdk/build64
+
+
